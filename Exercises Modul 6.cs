3559617ef2c5a8af999e1c763d8ddcefe4cee8f9
agencyadmin <osmprobox@gmail.com>
2021-12-24 12:46:06 +0300

    Модуль 7. Упражнения.

---
+++ Exercises Modul 6.cs
@@ -0,0 +1,18 @@
+class Employee
+{
+    public string Name;
+    public int Age;
+    public int Salary;
+}
+
+class ProjectManager : Employee
+{
+    public string ProgectName;
+
+}
+
+class Developer : Employee
+{
+    public string ProgrammingLanguage;
+
+}
