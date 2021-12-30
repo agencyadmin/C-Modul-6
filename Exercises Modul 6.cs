f0e1095f4b5ac42eec8d7d4a4c59fee14f8b0c9f
agencyadmin <osmprobox@gmail.com>
2021-12-30 12:56:43 +0300

    Задание 7.2.7

--- Exercises Modul 6.cs
+++ Exercises Modul 6.cs
@@ -83,7 +83,86 @@ class Obj
             this.name = name;
             owner = ownerName;
             length = objLength;
-            this.count = count; 
+            this.count = count;
         }
     }
+
+    class BaseClass
+    {
+        protected string Name;
+
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public string Description;
+
+        public int Counter;
+
+        CerivedClass(string name, string description) : base(name)
+        {
+                     protected string Description = description;
+    }
+
+    CerivedClass(string name, string description, int counter) : this CerivedClass(name, description)
+    {
+                 protected string Counter = counter;
+}
+    }
+
+class A
+{
+    public virtual void Display()
+    {
+        Console.WriteLine("Вызван метод класса A");
+    }
+}
+class B : A
+{
+    public override void Display()
+    {
+        Console.WriteLine("Вызван метод класса B");
+    }
+}
+class C : A
+{
+    public new void Display()
+    {
+        Console.WriteLine("Вызван метод класса C");
+    }
+    class D : B
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Вызван метод класса D");
+        }
+    }
+
+    class E : C
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Вызван метод класса E");
+        }
+    }
+}
+
+public static void Main()
+{
+    A a = new A();
+    B b = new B();
+    C c = new C();
+
+    a.Display();    // метод класса A
+    b.Display();    // метод класса B
+    ((A)b).Display();   // метод класса B
+
+    c.Display();    // метод класса C
+    ((A)c).Display();   // метод класса B
+    ((B)c).Display();   // метод класса B
+}
 }
