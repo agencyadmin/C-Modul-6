9b5fc43d6118900215743d9815b2d6932c1f95fb
agencyadmin <osmprobox@gmail.com>
2021-12-30 13:13:37 +0300

    задание 7.2.8

---
+++ Tasks7/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+class Programm
+{
+    class A
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Вызван метод класса A");
+        }
+    }
+    class B : A
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Вызван метод класса B");
+        }
+    }
+    class C : A
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Вызван метод класса C");
+        }
+    }
+    class D : B
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Вызван метод класса D");
+        }
+    }
+
+    class E : C
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Вызван метод класса E");
+        }
+    }
+    public static void Main(string[] arg)
+    {
+        D d = new D();
+        E e = new E();
+
+        d.Display();
+        ((A)e).Display();
+        ((B)d).Display();
+        ((A)d).Display();
+    }
+}
